87cca9d02670f7f3e88b1af693492f302809fb76
TimofeyV <vazhenin.t@gmail.com>
2023-04-14 21:51:45 +0300

    work1

---
+++ 001/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите число: ");
+string number = Console.ReadLine();
+if (number.Length != 5) Console.WriteLine("Введите правильно число");
+else
+{
+    if (number[0] == number[4] && number[1] == number[3]) Console.WriteLine("Ваше число палиндром");
+    else Console.WriteLine("Число не является палиндромом");
+}
+    
